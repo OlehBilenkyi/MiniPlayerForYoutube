9b1252e8c760df5b6305b3454754cff1d3dddbe4
Oleh Bilenkyi <i3eLuy@gmail.com>
2025-06-07 18:49:51 +0200

    update

--- src/components/Visualizer/VisualizerToggle/VisualizerToggle.tsx
+++ src/components/Visualizer/VisualizerToggle/VisualizerToggle.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Visualizer from "../Visualizer";
-import "./YouTubeAudioPlayer.scss";
+import "./VisualizerToggle.scss";
 
 interface VisualizerToggleProps {
   show: boolean;
